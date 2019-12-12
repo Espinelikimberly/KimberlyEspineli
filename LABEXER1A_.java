fb2812cbd2a25c67919b102e5675a82085e7d7cd
Espinelikimberly <58801558+Espinelikimberly@users.noreply.github.com>
2019-12-12 14:32:01 +0800

    Add files via upload

---
+++ LABEXER1A_.java
@@ -0,0 +1,13 @@
+public class LABEXER1A_ {
+    
+    public static void main(String[] args) {
+    	int faveNumber= 17;
+    	String faveCarChar= " Spongebob!";
+    	char mi= 'G';
+    	char [] nickNameArray= {'K', 'i', 'm', 'b', 'e', 'r', 'l', 'y'};
+    	System.out.println(faveNumber + " is my favorite number. ");
+    	System.out.println("I Love" + faveCarChar);
+    	System.out.println("My name is Ruth Kimberly " + mi + " Espineli. ");
+    	System.out.println("You can call me " + nickNameArray [0] + nickNameArray [1] + nickNameArray [2] + nickNameArray [3] + nickNameArray [4] + nickNameArray [5] + nickNameArray [6] + nickNameArray [7]);	
+    }
+}
